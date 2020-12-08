2039af2052ccc7d90a628d0ea2db3da6bd800950
Ignat Zakrevsky <iezakrevsky@gmail.com>
2020-12-08 08:49:34 +0300

    Add AoC 2020 Day 8

---
+++ 2020_08/solution.rb
@@ -0,0 +1,54 @@
+input = File.read('input.txt')
+
+commands =
+  input
+    .split("\n")
+    .map { |command| command.split(' ') }
+    .map { |(instruction, value)| [instruction, value.to_i] }
+
+def valid?(commands)
+  acc = 0
+  cursor = 0
+  calls_tracker = Array.new(commands.length, 0)
+
+  halted = loop {
+    instruction, value = commands[cursor]
+    calls_tracker[cursor] += 1
+
+    break false if calls_tracker.any? { _1 && _1 > 1 }
+
+    case instruction
+    when 'nop'
+      cursor += 1
+    when 'acc'
+      acc += value
+      cursor += 1
+    when 'jmp'
+      cursor += value
+    end
+
+    break true if cursor == commands.length
+  }
+
+  [halted, acc]
+end
+
+_, puzzle1 = valid?(commands)
+
+puzzle2 = commands.each_with_index { |(instruction, value), index|
+  alternative = case instruction
+  when 'nop' then 'jmp'
+  when 'jmp' then 'nop'
+  end
+
+  next if alternative.nil?
+
+  alternative_commands = commands.dup
+  alternative_commands[index] = [alternative, value]
+
+  halted, result = valid?(alternative_commands)
+
+  break result if halted
+}
+
+p [puzzle1, puzzle2]
