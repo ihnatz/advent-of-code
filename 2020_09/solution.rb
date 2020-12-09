3d0bab57eae826db60ba5a42546c4133e66baf47
Ignat Zakrevsky <iezakrevsky@gmail.com>
2020-12-09 08:40:49 +0300

    Add AoC 2020 Day 9

---
+++ 2020_09/solution.rb
@@ -0,0 +1,23 @@
+PREABMLE_LENGTH = 25
+
+input = File.read('input.txt').split("\n").map(&:to_i)
+
+preabmle = input.slice(0, PREABMLE_LENGTH)
+puzzle1 = input.drop(PREABMLE_LENGTH).each { |next_number|
+  break next_number unless preabmle.combination(2).map(&:sum).any? { _1 == next_number }
+  preabmle.shift
+  preabmle.push(next_number)
+}
+
+puzzle2 = input.length.times { |i|
+  window_size = 1
+  sequence = loop {
+    window = input.drop(i).slice(0, window_size)
+    break window if window.sum == puzzle1
+    break nil if window.sum > puzzle1
+    window_size += 1
+  }
+  break(sequence.min + sequence.max) if sequence
+}
+
+p [puzzle1, puzzle2]
