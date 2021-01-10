8102e24465cd967adeb37bc02200ea7cddcf3717
Ignat Zakrevsky <iezakrevsky@gmail.com>
2021-01-10 14:46:16 +0300

    Add day 13 solution with CRT

--- 2020_13/solution.rb
+++ 2020_13/solution.rb
@@ -1,5 +1,31 @@
 OUT_OF_SERVICE = "x"
 
+def extended_gcd(a, b)
+  last_remainder, remainder = a.abs, b.abs
+  x, last_x, y, last_y = 0, 1, 1, 0
+  while remainder != 0
+    last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
+    x, last_x = last_x - quotient*x, x
+    y, last_y = last_y - quotient*y, y
+  end
+
+  return last_remainder, last_x * (a < 0 ? -1 : 1)
+end
+
+def invmod(e, et)
+  g, x = extended_gcd(e, et)
+  raise 'The maths are broken!' if g != 1
+  x % et
+end
+
+def crt(system)
+  n = system.map(&:last).inject(:*)
+  ys = system.map(&:last).map { |it| n / it }
+  zs = system.map(&:last).zip(ys).map { |a, m| invmod(m, a) }
+  x = system.map(&:first).zip(ys, zs).map { |it| it.inject(&:*) }.sum % n
+  [x, n]
+end
+
 input = File.read("input.txt")
 timestamp, buses = input.split("\n")
 buses = buses.split(",").map { |description|
@@ -23,9 +49,9 @@ def valid?(buses, timestamp)
   }
 end
 
-def find_earlies_time(buses, start = 0, step = 1)
+def find_earliest_time(buses, start = 0, step = 1)
   return [buses.first, buses.first] if buses.length == 1
-  start, step = find_earlies_time(buses.slice(0..-2), start, step)
+  start, step = find_earliest_time(buses.slice(0..-2), start, step)
 
   next_tick = start
 
@@ -38,6 +64,11 @@ def find_earlies_time(buses, start = 0, step = 1)
   [earliest_timestamp, max_step]
 end
 
-puzzle2 = find_earlies_time(buses).first
+puzzle2 = crt(buses.map.with_index { |val, idx|
+  next if val == OUT_OF_SERVICE
+  [idx, val.to_i]
+}.compact).reverse.inject(:-)
+
+puzzle2 = find_earliest_time(buses).first
 
 p [puzzle1, puzzle2]
