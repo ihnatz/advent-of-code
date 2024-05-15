ee2284bf3d1ffece28fa31b8187a64772f417e21
Ignat Zakrevsky <iezakrevsky@gmail.com>
2024-05-15 08:52:13 +0100

    Add Day 04

---
+++ aoc-2017/day04/solution.py
@@ -0,0 +1,10 @@
+content = open("input.txt").readlines()
+
+def canon(word): return ''.join(sorted(word))
+def words(line): return line.strip().split(" ")
+
+print(sum(len(words) == len(set(words))
+  for words in [words(line) for line in content]))
+
+print(sum(len(words) == len(set(words))
+  for words in [list(map(canon, words(line))) for line in content]))
